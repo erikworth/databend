3f27293c286077d51868d79da9a3460d01322a35
nosubsgo <erik.worth@gmail.com>
2013-11-19 15:27:34 -0800

    now loops

--- databend.py
+++ databend.py
@@ -3,32 +3,34 @@ from random import *
 import string
  
  
-def data_bend(input_file, output_file):
+def data_bend(input_file):
     # Open file in binary mode
     f = open(input_file, "rb")
     data = f.read()
     f.close()
  
-    # preserve header data
-    header = data[:500]
-    core = data[500:]
-    data_size = len(core)
 
-    randstring = ''
 
-    #core = pre + sample + post
+    
 
-    for x in range(randint(1,10)):    
-        for y in range(randint(1,300)):
-            randstring += choice(string.letters + string.digits)
+    for x in range(50):
+        #preserve header data
+        header = data[:500]
+        core = data[500:]
+        randstring = ''
+        
+        #write random strings a random number of times into image data
+        for y in range(randint(1,5)):    
+            for z in range(randint(1,100)):
+                randstring += choice(string.letters + string.digits)
 
-        corelist = list(core)
-        corelist.insert(randint(0, data_size - 1), randstring)
-        core = ''.join(corelist)
+            corelist = list(core)
+            corelist.insert(randint(0, data_size - 1), randstring)
+            core = ''.join(corelist)
         
  
         
  
-    f = open(output_file, "wb")
-    f.write(header + core)
-    f.close()
+        f = open('%s'%(x)+input_file, "wb")
+        f.write(header + core)
+        f.close()
