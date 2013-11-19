2b15c3e14da861d15c72f20fcba8bcee0fb37aaa
nosubsgo <erik.worth@gmail.com>
2013-11-19 12:30:10 -0800

    First draft

---
+++ databend.py
@@ -0,0 +1,33 @@
+import os
+from random import *
+import string
+ 
+ 
+def data_bend(input_file, output_file):
+    # Open file in binary mode
+    f = open(input_file, "rb")
+    data = f.read()
+    f.close()
+ 
+    # preserve header data
+    header = data[:500]
+    core = data[500:]
+
+    randstring = ''
+
+    #core = pre + sample + post
+
+    for x in range(randint(1,10)):    
+        for y in range(randint(1,150)):
+            randstring += choice(string.letters + string.digits)
+
+        corelist = list(core)
+        corelist.insert(randint(0, data_size - 1), randstring)
+        core = ''.join(corelist)
+        
+ 
+        
+ 
+    f = open(output_file, "wb")
+    f.write(header + core)
+    f.close()
