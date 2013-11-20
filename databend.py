13c93cc103a5149e6aee5b175add6c87b05be916
nosubsgo <erik.worth@gmail.com>
2013-11-19 16:09:59 -0800

    works again

--- databend.py
+++ databend.py
@@ -21,11 +21,11 @@ def data_bend(input_file):
         
         #write random strings a random number of times into image data
         for y in range(randint(1,5)):    
-            for z in range(randint(1,100)):
+            for z in range(randint(1,80)):
                 randstring += choice(string.letters + string.digits)
 
             corelist = list(core)
-            corelist.insert(randint(0, data_size - 1), randstring)
+            corelist.insert(randint(0, len(core) - 1), randstring)
             core = ''.join(corelist)
         
  
